d4440cbcd5a0d4c38c710cde984880c4a3088ec8
Im-Abhi <abhishekgupta942001@gmail.com>
2022-07-09 22:51:51 +0530

    Users-list-page

---
+++ src/user/components/UserItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './UserItem.css';
+
+const UserItem = (props) => {
+    return (
+        <></>
+    )
+}
+
+export default UserItem;
---
+++ src/user/components/UsersList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import UserItem from "./UserItem";
+import './UsersList.css';
+
+const UsersList = (props) => {
+    return (
+        (props.items.length === 0) ?
+            <div className="center">
+                <h2>No Users Found!</h2>
+            </div>
+            :
+            <ul>
+                {props.items.map(user => {
+                    return (
+                        <UserItem
+                            key={user.id}
+                            id={user.id}
+                            image={user.image}
+                            name={user.name}
+                            placeCount={user.places}
+                        />
+                    )
+                })}
+            </ul>
+    )
+}
+
+export default UsersList;
