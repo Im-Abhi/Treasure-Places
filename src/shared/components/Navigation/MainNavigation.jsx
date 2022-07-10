577a8446ca81164d5d155b7d6177ce37fe458bee
Im-Abhi <abhishekgupta942001@gmail.com>
2022-07-10 20:31:14 +0530

    animated the side-drawer

--- src/shared/components/Navigation/MainNavigation.jsx
+++ src/shared/components/Navigation/MainNavigation.jsx
@@ -9,27 +9,24 @@ import SideDrawer from "./SideDrawer";
 
 const MainNavigation = (props) => {
     const [drawerIsOpen, setDrawerIsOpen] = useState(false);
-    const openDrawer = () => {
+    const openDrawerHandler = () => {
         setDrawerIsOpen(true);
     }
-    const closeDrawer = () => {
+    const closeDrawerHandler = () => {
         setDrawerIsOpen(false);
     }
     return (
         <Fragment>
             {
-                drawerIsOpen && <BackDrop onClick={closeDrawer} />
+                drawerIsOpen && <BackDrop onClick={closeDrawerHandler} />
             }
-            {
-                drawerIsOpen &&
-                <SideDrawer>
-                    <nav className="main-navigation__drawer-nav">
-                        <NavLinks />
-                    </nav>
-                </SideDrawer>
-            }
+            <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+                <nav className="main-navigation__drawer-nav">
+                    <NavLinks />
+                </nav>
+            </SideDrawer>
             <MainHeader>
-                <button className="main-navigation__menu-btn" onClick={openDrawer}>
+                <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
                     <span></span>
                     <span></span>
                     <span></span>
