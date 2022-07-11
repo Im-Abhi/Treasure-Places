e9a6fa0cf3657e462ea85a5f9d0827df7d592e5d
Im-Abhi <abhishekgupta942001@gmail.com>
2022-07-11 21:03:34 +0530

    rendering user places, dynamic route segment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import Users from './user/pages/Users';
 
@@ -12,6 +13,7 @@ const App = () => {
 				<Routes>
 					<Route path='/' element={<Users />} exact />
 					<Route path='/places/new' element={<NewPlace />} />
+					<Route path='/:userId/places' element={<UserPlaces />} exact />
 					{/* <Redirect to='/' /> */}
 				</Routes>
 			</main>
