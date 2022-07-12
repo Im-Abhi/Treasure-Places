8b6722dc31b4af081d1f89a49b82281fae123659
Im-Abhi <abhishekgupta942001@gmail.com>
2022-07-12 13:11:17 +0530

    Add input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Input from "../../shared/components/FormElements/Input";
+
+import './NewPlace.css'
 
 const NewPlace = () => {
     return (
-        <h2>
-            NewPlace Works!
-        </h2>
+        <form className="place-form">
+            <Input
+                element="input"
+                type="text"
+                label="title"
+            />
+        </form>
     )
 }
 
---
+++ src/shared/components/FormElements/Input.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import './Input.css';
+
+const Input = (props) => {
+    const element = props.element === 'input' ?
+        <input
+            id={props.id}
+            type={props.type}
+            placeholder={props.placeholder}
+        /> :
+        <textarea
+            id={props.id}
+            rows={props.rows || 3}
+            style={{ "resize": "none" }}
+        />
+
+
+    return (
+        <div className={`form-control`}>
+            <label htmlFor={props.id}>{props.label}</label>
+            {element}
+        </div>
+    )
+}
+
+export default Input;
