4790d95bc07bb95802c7c05641e2151bfe00872a
Im-Abhi <abhishekgupta942001@gmail.com>
2022-07-16 20:33:53 +0530

    setting up auth-context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import NewPlace from './places/pages/NewPlace';
 import UpdatePlace from './places/pages/UpdatePlace';
@@ -7,21 +7,40 @@ import MainNavigation from './shared/components/Navigation/MainNavigation';
 import Auth from './user/pages/Auth';
 import Users from './user/pages/Users';
 
+import { AuthContext } from './shared/context/auth-context';
+
 const App = () => {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	const login = useCallback(() => {
+		setIsLoggedIn(true);
+	});
+
+	const logout = useCallback(() => {
+		setIsLoggedIn(false);
+	});
+
+	useContext(AuthContext);
 	return (
-		<Router>
-			<MainNavigation />
-			<main>
-				<Routes>
-					<Route path='/' element={<Users />} exact />
-					<Route path='/:userId/places' element={<UserPlaces />} exact />
-					<Route path='/places/new' element={<NewPlace />} />
-					<Route path='/places/:placeId' element={<UpdatePlace />} />
-					<Route path='/auth' element={<Auth />} />
-					{/* <Redirect to='/' /> */}
-				</Routes>
-			</main>
-		</Router>
+		<AuthContext.Provider value={{
+			isLoggedIn: isLoggedIn,
+			login: login,
+			logout: logout
+		}}>
+			<Router>
+				<MainNavigation />
+				<main>
+					<Routes>
+						<Route path='/' element={<Users />} exact />
+						<Route path='/:userId/places' element={<UserPlaces />} exact />
+						<Route path='/places/new' element={<NewPlace />} />
+						<Route path='/places/:placeId' element={<UpdatePlace />} />
+						<Route path='/auth' element={<Auth />} />
+						{/* <Redirect to='/' /> */}
+					</Routes>
+				</main>
+			</Router>
+		</AuthContext.Provider>
 	)
 }
 
