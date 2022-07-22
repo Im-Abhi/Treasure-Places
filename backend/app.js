c7efe3bc2c690fcd1b9851397ad0572fa3915a0b
Im-Abhi <abhishekgupta942001@gmail.com>
2022-07-22 13:49:19 +0530

    backend setup + implement basic routing

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const placesRoutes = require('./routes/places-routes');
+
+const app = express();
+
+app.use(placesRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server Started Successfully on PORT ${PORT}`);
+});
