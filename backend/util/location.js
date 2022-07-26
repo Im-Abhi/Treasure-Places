3cbb7405a21d171282102dd1991d696b32876ad3
Im-Abhi <abhishekgupta942001@gmail.com>
2022-07-26 19:20:58 +0530

    using google geo api

---
+++ backend/util/location.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+const HttpError = require("../models/http-error");
+
+const API_KEY = 'AIzaSyC_ANbi6xo4ydjzOWs_EtWYm7R0dFMgHNs';
+
+const getCoordsForAddress = async (address) => {
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`);
+    const { data } = await response;
+
+    if (!data || data.status === 'ZERO_RESULTS') {
+        throw new HttpError('Could not find location for the specified address.', 422);
+    }
+
+    const coordinates = data.results[0].geometry.location;
+
+    return coordinates;
+}
+
+module.exports = getCoordsForAddress;
