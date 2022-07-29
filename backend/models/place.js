2bd5556e433ccb326e9023644c79a006db424e3a
Im-Abhi <abhishekgupta942001@gmail.com>
2022-07-29 17:35:33 +0530

    Place schema+model

---
+++ backend/models/place.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const placeSchema = {
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    location: {
+        lat: {
+            type: Number,
+            required: true,
+        },
+        lng: {
+            type: Number,
+            required: true,
+        },
+    },
+    creator: {
+        type: String,
+        required: true,
+    },
+}
+
+module.exports = model('Place', placeSchema);
