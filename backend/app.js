e025bba9d25e6b1fdd409b813d6a3cbdcd2f611d
Im-Abhi <abhishekgupta942001@gmail.com>
2022-07-29 17:29:52 +0530

    Connect with mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config()
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -28,6 +30,13 @@ app.use((error, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server Started Successfully on PORT ${PORT}`);
-});
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server Started Successfully on PORT ${PORT}`);
+        });
+    }).
+    catch(err => {
+        console.log(err);
+    });
