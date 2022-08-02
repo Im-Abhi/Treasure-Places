f2e6f3f77678ad055ae4f46d969a2ef2ba2c1422
Im-Abhi <abhishekgupta942001@gmail.com>
2022-08-02 22:09:20 +0530

    Remove dummy data

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -6,20 +6,6 @@ const getCoordsForAddress = require('../util/location');
 const Place = require('../models/place');
 const User = require('../models/user');
 
-let DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        location: {
-            lat: 40.7484474,
-            lng: -73.9871516
-        },
-        address: '20 W 34th St, New York, NY 10001',
-        creator: 'u1'
-    }
-];
-
 const getPlaceById = async (req, res, next) => {
     const placeId = req.params.pid;
 
@@ -47,6 +33,7 @@ const getPlacesByUserId = async (req, res, next) => {
         return next(new HttpError('Fetching places failed, please try again later', 500));
     }
 
+    // if(places || places.length === 0)
     if (!userWithPlaces || userWithPlaces.places.length === 0) {
         return next(new HttpError('Could not find places for the provided user id.', 404));
     }
--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -3,15 +3,6 @@ const { validationResult } = require('express-validator');
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
 
-const DUMMY_USERS = [
-    {
-        id: 'u1',
-        name: 'Max Schwarz',
-        email: 'test@test.com',
-        password: 'testers'
-    }
-]
-
 const getUsers = async (req, res, next) => {
     let users;
     try {
