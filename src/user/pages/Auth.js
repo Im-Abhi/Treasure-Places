4b871e48ac8da8ab2773eb6e33d55912e053c59a
Im-Abhi <abhishekgupta942001@gmail.com>
2022-08-02 22:46:20 +0530

    try sending first request to backend

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -47,10 +47,29 @@ const Auth = () => {
         setIsLoginMode(prev => !prev);
     }
 
-    const authSubmitHandler = event => {
+    const authSubmitHandler = async (event) => {
         event.preventDefault();
+
+        if (isLoginMode) {
+        } else {
+            try {
+                const response = await fetch('http://localhost:5000/api/users/signup', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        name: formState.inputs.name.value,
+                        email: formState.inputs.email.value,
+                        password: formState.inputs.password.value,
+                    })
+                });
+                const responseData = await response.json();
+            } catch (error) {
+                console.log(error);
+            }
+        }
         auth.login();
-        console.log(formState.inputs);
     };
 
     return (
