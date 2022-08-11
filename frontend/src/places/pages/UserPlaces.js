55282a6ea7bca4433e744e05fd7472745343893b
Im-Abhi <abhishekgupta942001@gmail.com>
2022-08-11 21:01:13 +0530

    changes for production

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -14,7 +14,7 @@ const UserPlaces = () => {
     useEffect(() => {
         const fetchPlaces = async () => {
             try {
-                const responseData = await sendRequest(`http://localhost:5000/api/places/user/${userId}`);
+                const responseData = await sendRequest(`${process.env.REACT_APP_BACKEND_URL}/places/user/${userId}`);
                 setLoadedPlaces(responseData.places);
             } catch (err) {
 
