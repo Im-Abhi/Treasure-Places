cd84c5aa2112f40f339ca1a9dbdf1e74b55660f7
Im-Abhi <abhishekgupta942001@gmail.com>
2022-08-11 21:28:56 +0530

    code spilting for production

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom'
-import NewPlace from './places/pages/NewPlace';
-import UpdatePlace from './places/pages/UpdatePlace';
-import UserPlaces from './places/pages/UserPlaces';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
-import Auth from './user/pages/Auth';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+
+// import NewPlace from './places/pages/NewPlace';
+// import UpdatePlace from './places/pages/UpdatePlace';
+// import UserPlaces from './places/pages/UserPlaces';
+// import MainNavigation from './shared/components/Navigation/MainNavigation';
+// import Auth from './user/pages/Auth';
 import Users from './user/pages/Users';
+
 
 import { AuthContext } from './shared/context/auth-context';
 import { useAuth } from './shared/hooks/auth-hook';
+import LoadingSpinner from './shared/components/UIElements/LoadingSpinner';
+
+const NewPlace = lazy(() => import('./places/pages/NewPlace'));
+const UpdatePlace = lazy(() => import('./places/pages/UpdatePlace'));
+const UserPlaces = lazy(() => import('./places/pages/UserPlaces'));
+const MainNavigation = lazy(() => import('./shared/components/Navigation/MainNavigation'));
+const Auth = lazy(() => import('./user/pages/Auth'));
 
 const App = () => {
 	const { token, login, logout, userId } = useAuth();
@@ -17,22 +26,22 @@ const App = () => {
 
 	if (token) {
 		routes = (
-			<Fragment>
+			<Routes>
 				<Route path='/' element={<Users />} exact />
 				<Route path='/:userId/places' element={<UserPlaces />} exact />
 				<Route path='/places/new' element={<NewPlace />} />
 				<Route path='/places/:placeId' element={<UpdatePlace />} />
 				<Route path='*' element={<Navigate to='/' replace />} />
-			</Fragment>
+			</Routes>
 		);
 	} else {
 		routes = (
-			<Fragment>
+			<Routes>
 				<Route path='/' element={<Users />} exact />
 				<Route path='/:userId/places' element={<UserPlaces />} exact />
 				<Route path='/auth' element={<Auth />} />
 				<Route path='*' element={<Navigate to='/auth' replace />} />
-			</Fragment>
+			</Routes>
 		);
 	}
 
@@ -49,10 +58,13 @@ const App = () => {
 			<Router>
 				<MainNavigation />
 				<main>
-					<Routes>
+					<Suspense fallback={
+						<div className='center'>
+							<LoadingSpinner />
+						</div>
+					}>
 						{routes}
-						{/* <Redirect to='/' /> */}
-					</Routes>
+					</Suspense>
 				</main>
 			</Router>
 		</AuthContext.Provider>
